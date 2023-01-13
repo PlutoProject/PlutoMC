1b75963c900c4df822148ed86e5540eeb0116cfe
nostalgic853 <yuu8583@proton.me>
2023-01-13 12:53:29 +0800

    chore: 添加注释。

--- framework/src/main/java/ltd/kumo/plutomc/framework/platform/ProxyPlatform.java
+++ framework/src/main/java/ltd/kumo/plutomc/framework/platform/ProxyPlatform.java
@@ -4,6 +4,10 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import ltd.kumo.plutomc.framework.AbstractPlatform;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * 代理端 platform.
+ */
+@SuppressWarnings("unused")
 public class ProxyPlatform extends AbstractPlatform<ProxyServer> {
     public ProxyPlatform(ProxyServer server) {
         super(server);
