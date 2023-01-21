9a7ee42ade393d9aa31439f3b1b690bce4f94c9a
DeeChael <DeeChael@outlook.com>
2023-01-21 22:35:46 +0800

    fix: bukkit argument message

--- framework-bukkit/src/main/java/ltd/kumo/plutomc/framework/bukkit/command/BukkitCommandReflections.java
+++ framework-bukkit/src/main/java/ltd/kumo/plutomc/framework/bukkit/command/BukkitCommandReflections.java
@@ -1,6 +1,7 @@
 package ltd.kumo.plutomc.framework.bukkit.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.CommandNode;
 import ltd.kumo.plutomc.framework.bukkit.utilities.reflect.*;
 import org.bukkit.command.Command;
@@ -21,6 +22,8 @@ public final class BukkitCommandReflections {
     public final static RawClass<?> CLASS_CRAFT_SERVER = RawClass.of(Ref.obcClass("CraftServer"));
     public final static RawClass<?> CLASS_ENTITY_PLAYER = RawClass.of(Ref.nmsClass("server.level.EntityPlayer"));
     public final static RawClass<?> CLASS_CRAFT_PLAYER = RawClass.of(Ref.obcClass("entity.CraftPlayer"));
+    public final static RawClass<?> CLASS_I_CHAT_BASE_COMPONENT = RawClass.of(Ref.nmsClass("network.chat.IChatBaseComponent"));
+    public final static RawClass<?> CLASS_ARGUMENT_CHAT = RawClass.of(Ref.nmsClass("commands.arguments.ArgumentChat"));
 
     public final static RawConstructor<Command> CONSTRUCTOR_VANILLA_COMMAND_WRAPPER;
 
@@ -36,6 +39,8 @@ public final class BukkitCommandReflections {
     public final static RawMethod METHOD_HAS_PERMISSION_2;
     public final static RawMethod METHOD_GET_BUKKIT_SENDER;
     public final static RawMethod METHOD_GET_BUKKIT_PLAYER;
+    public final static RawMethod METHOD_GET_STRING;
+    public final static RawMethod METHOD_GET_MESSAGE;
 
     static {
         CONSTRUCTOR_VANILLA_COMMAND_WRAPPER = (RawConstructor<Command>) CLASS_VANILLA_COMMAND_WRAPPER.findConstructor(CLASS_COMMAND_DISPATCHER.original(), CommandNode.class);
@@ -52,6 +57,8 @@ public final class BukkitCommandReflections {
         METHOD_HAS_PERMISSION_2 = CLASS_COMMAND_LISTENER_WRAPPER.findMethod(false, boolean.class, int.class, String.class);
         METHOD_GET_BUKKIT_SENDER = CLASS_COMMAND_LISTENER_WRAPPER.findMethod(false, CommandSender.class);
         METHOD_GET_BUKKIT_PLAYER = CLASS_ENTITY_PLAYER.findMethod(false, CLASS_CRAFT_PLAYER.original());
+        METHOD_GET_STRING = CLASS_I_CHAT_BASE_COMPONENT.findMethod(false, String.class);
+        METHOD_GET_MESSAGE = CLASS_ARGUMENT_CHAT.findMethod(true, CLASS_I_CHAT_BASE_COMPONENT.original(), CommandContext.class, String.class);
     }
 
 }
--- framework-bukkit/src/main/java/ltd/kumo/plutomc/framework/bukkit/command/brigadier/Message.java
+++
@@ -1,4 +0,0 @@
-package ltd.kumo.plutomc.framework.bukkit.command.brigadier;
-
-public record Message(String content) {
-}
