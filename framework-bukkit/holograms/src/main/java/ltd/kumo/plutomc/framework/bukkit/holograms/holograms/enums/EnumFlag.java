19b5c755c1ff9eb99721077a6bb088e545bb71bd
nostalgic853 <yuu8583@proton.me>
2023-01-25 01:00:28 +0800

    chore: remove unlicensed parts

--- framework-bukkit/holograms/src/main/java/ltd/kumo/plutomc/framework/bukkit/holograms/holograms/enums/EnumFlag.java
+++
@@ -1,8 +0,0 @@
-package ltd.kumo.plutomc.framework.bukkit.holograms.holograms.enums;
-
-public enum EnumFlag {
-    DISABLE_PLACEHOLDERS,
-    DISABLE_UPDATING,
-    DISABLE_ANIMATIONS,
-    DISABLE_ACTIONS
-}
